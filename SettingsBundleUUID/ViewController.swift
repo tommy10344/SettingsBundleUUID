f617117fae1f7d8e9cad991e81c246dcf8b79d86
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2020-07-16 22:59:31 +0900

    Initial Commit

---
+++ SettingsBundleUUID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsBundleUUID
+//
+//  Created by Hiroaki Tomiyoshi on 2020/07/16.
+//  Copyright © 2020 Hiroaki Tomiyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
